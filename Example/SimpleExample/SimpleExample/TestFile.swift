ebb4b0e03d62a6dcede5ca96ffaa0f61bdf3946f
MoveoOne Integration <integration@moveo.one>
2025-04-03 14:39:36 +0200

    MoveoOne automatic integration.

--- Example/SimpleExample/SimpleExample/TestFile.swift
+++ Example/SimpleExample/SimpleExample/TestFile.swift
@@ -5,3 +5,77 @@
 //  Created by Pavle Rogan on 3.4.25..
 //
 
+import SwiftUI
+import MoveoOne
+
+struct TestView: View {
+    let SEMANTIC_GROUP = "example_app"
+
+    var body: some View {
+        VStack {
+            Text("Sample Text")
+                .onAppear {
+                    MoveoOne.instance.tick(
+                        moveoOneData: MoveoOneData(
+                            semanticGroup: SEMANTIC_GROUP,
+                            id: "sample_text",
+                            type: .text,
+                            action: .view,
+                            value: "Sample Text",
+                            metadata: [:]
+                        )
+                    )
+                }
+            
+            Button("Sample Button") {
+                MoveoOne.instance.tick(
+                    moveoOneData: MoveoOneData(
+                        semanticGroup: SEMANTIC_GROUP,
+                        id: "sample_button",
+                        type: .button,
+                        action: .click,
+                        value: "Sample Button",
+                        metadata: [:]
+                    )
+                )
+            }
+        }
+    }
+}
+import SwiftUI
+import MoveoOne
+
+struct TestView: View {
+    let SEMANTIC_GROUP = "example_app"
+
+    var body: some View {
+        VStack {
+            Text("Sample Text")
+                .onAppear {
+                    MoveoOne.instance.tick(
+                        moveoOneData: MoveoOneData(
+                            semanticGroup: SEMANTIC_GROUP,
+                            id: "sample_text",
+                            type: .text,
+                            action: .view,
+                            value: "Sample Text",
+                            metadata: [:]
+                        )
+                    )
+                }
+            
+            Button("Sample Button") {
+                MoveoOne.instance.tick(
+                    moveoOneData: MoveoOneData(
+                        semanticGroup: SEMANTIC_GROUP,
+                        id: "sample_button",
+                        type: .button,
+                        action: .click,
+                        value: "Sample Button",
+                        metadata: [:]
+                    )
+                )
+            }
+        }
+    }
+}
