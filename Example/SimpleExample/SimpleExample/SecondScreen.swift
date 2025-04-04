d70933bd5a9a19b57f985fe0951d2f7c3681a408
MoveoOne Integration <integration@moveo.one>
2025-04-04 13:15:59 +0200

    MoveoOne automatic integration.

--- Example/SimpleExample/SimpleExample/SecondScreen.swift
+++ Example/SimpleExample/SimpleExample/SecondScreen.swift
@@ -13,6 +13,18 @@ struct SecondScreen: View {
             Text("This is second screen")
                 .font(.largeTitle)
                 .padding()
+                .onAppear {
+                    MoveoOne.instance.tick(
+                        moveoOneData: MoveoOneData(
+                            semanticGroup: "second_screen",
+                            id: "second_screen_text",
+                            type: .text,
+                            action: .view,
+                            value: "This is second screen",
+                            metadata: [:]
+                        )
+                    )
+                }
         }
         .navigationBarTitle("Second Screen", displayMode: .inline)
     }
