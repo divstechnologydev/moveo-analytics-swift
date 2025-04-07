0fe39a868203f86ff3b6d805c24685188065e0d1
MoveoOne Integration <integration@moveo.one>
2025-04-07 12:59:01 +0200

    MoveoOne automatic integration.

--- Example/SimpleExample/SimpleExample/TestFile.swift
+++ Example/SimpleExample/SimpleExample/TestFile.swift
@@ -1,7 +1,68 @@
-//
-//  TestFile.swift
-//  SimpleExample
-//
-//  Created by Pavle Rogan on 3.4.25..
-//
+import SwiftUI
+import MoveoOne
 
+struct TestFile: View {
+    let SEMANTIC_GROUP = "test_file"
+
+    var body: some View {
+        VStack {
+            Text("Welcome to Test File")
+                .font(.title)
+                .padding()
+                .onAppear {
+                    MoveoOne.instance.tick(
+                        moveoOneData: MoveoOneData(
+                            semanticGroup: SEMANTIC_GROUP,
+                            id: "test_file_text",
+                            type: .text,
+                            action: .view,
+                            value: "Welcome to Test File",
+                            metadata: [:]
+                        )
+                    )
+                }
+            Button("Click Me") {
+                MoveoOne.instance.tick(
+                    moveoOneData: MoveoOneData(
+                        semanticGroup: SEMANTIC_GROUP,
+                        id: "test_file_button",
+                        type: .button,
+                        action: .click,
+                        value: "Click Me",
+                        metadata: [:]
+                    )
+                )
+            }
+        }
+        .onAppear {
+            MoveoOne.instance.tick(
+                moveoOneData: MoveoOneData(
+                    semanticGroup: SEMANTIC_GROUP,
+                    id: "test_file_screen",
+                    type: .screen,
+                    action: .appear,
+                    value: "Test File Screen",
+                    metadata: [:]
+                )
+            )
+        }
+        .onDisappear {
+            MoveoOne.instance.tick(
+                moveoOneData: MoveoOneData(
+                    semanticGroup: SEMANTIC_GROUP,
+                    id: "test_file_screen",
+                    type: .screen,
+                    action: .disappear,
+                    value: "Test File Screen",
+                    metadata: [:]
+                )
+            )
+        }
+    }
+}
+
+struct TestFile_Previews: PreviewProvider {
+    static var previews: some View {
+        TestFile()
+    }
+}
