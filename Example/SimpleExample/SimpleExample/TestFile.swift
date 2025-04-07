2ba5feab551f49f54c830a09b003c5de7b053419
MoveoOne Integration <integration@moveo.one>
2025-04-07 13:06:07 +0200

    MoveoOne automatic integration.

--- Example/SimpleExample/SimpleExample/TestFile.swift
+++ Example/SimpleExample/SimpleExample/TestFile.swift
@@ -1,7 +1,54 @@
-//
-//  TestFile.swift
-//  SimpleExample
-//
-//  Created by Pavle Rogan on 3.4.25..
-//
+import SwiftUI
+import MoveoOne
 
+struct TestFileView: View {
+    let SEMANTIC_GROUP = "test_file_screen"
+
+    var body: some View {
+        VStack {
+            Text("Test File Content")
+                .onAppear {
+                    MoveoOne.instance.tick(
+                        moveoOneData: MoveoOneData(
+                            semanticGroup: SEMANTIC_GROUP,
+                            id: "test_file_text",
+                            type: .text,
+                            action: .view,
+                            value: "Test File Content",
+                            metadata: [:]
+                        )
+                    )
+                }
+        }
+        .onAppear {
+            MoveoOne.instance.tick(
+                moveoOneData: MoveoOneData(
+                    semanticGroup: SEMANTIC_GROUP,
+                    id: "test_file_screen",
+                    type: .screen,
+                    action: .appear,
+                    value: "Test File Screen",
+                    metadata: [:]
+                )
+            )
+        }
+        .onDisappear {
+            MoveoOne.instance.tick(
+                moveoOneData: MoveoOneData(
+                    semanticGroup: SEMANTIC_GROUP,
+                    id: "test_file_screen",
+                    type: .screen,
+                    action: .disappear,
+                    value: "Test File Screen",
+                    metadata: [:]
+                )
+            )
+        }
+    }
+}
+
+struct TestFileView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestFileView()
+    }
+}
